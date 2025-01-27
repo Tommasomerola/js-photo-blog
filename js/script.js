92d3923941bd5d47875edbf904120ab48d576951
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-01-27 11:12:35 +0100

    fai logica js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Tramite api ottengo i dati che mi serviranno per riempire i campi delle card
+// https://lanciweb.github.io/demo/api/pictures/
+
+// dichiaro una variabile nella quale salverò i dai richiamati
+
+// effettuo la chiamata all api, gestendo i dati di ritorno 
+
+// tramite un ciclo for, prendo in cosiderazione ogni singolo 
+// elemento e ne estrapolo i dati
+
+// ricreo il formato html gia presente rimuovendo i dati specifici,  
+// in questo modo potro inserici singolarmente i dati per ogni card
+
+// assegno ogni rispettivo dato alla sua classe di appartenenza nel HTML
+
+// invio ogni volta la card completa all HTML tramite .innerHTML
+
+// mi assicuro di controllare anche un eventuale errore alla chiamata api
