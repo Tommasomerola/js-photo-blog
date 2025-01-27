d0209c9cbdf53580e56c4f834ce76b4739df3ed4
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-01-27 12:00:22 +0100

    fai gestione dati api e creazione card/ + rimuovi html di prova

--- js/script.js
+++ js/script.js
@@ -9,19 +9,34 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/")
 .then (response => {
     // salvo i dati richiamati in una variabile
     const data = response.data
-    console.log(data)
-})
-
-
+    //  console.log(data)
 
 // tramite un ciclo for, prendo in cosiderazione ogni singolo 
 // elemento e ne estrapolo i dati
+    for(let i = 0; i < data.length; i++) {
 
-// ricreo il formato html gia presente rimuovendo i dati specifici,  
-// in questo modo potro inserici singolarmente i dati per ogni card
+        // prendo in cosiderazione il singolo elemento 
+        const cardData = data [i];
 
-// assegno ogni rispettivo dato alla sua classe di appartenenza nel HTML
+        // ricreo il formato html gia presente rimuovendo i dati specifici,  
+        // in questo modo potro inserici singolarmente i dati per ogni card
+        const cardHTML = `
+            <div class="card">
+                <div class="photo">
+                    <img src="${cardData.url}" alt="" id="img">
+                    <img src="./img/pin.svg" alt="" id="pin">
+                </div>
+                
+                <div class="date">${cardData.date}</div>
+                <div class="text">${cardData.title}</div>
+            </div>`
+            ;
 
-// invio ogni volta la card completa all HTML tramite .innerHTML
+            // invio ogni volta la card completa all HTML tramite .innerHTML
+            container.innerHTML += cardHTML
+
+    }
+})
+
 
 // mi assicuro di controllare anche un eventuale errore alla chiamata api
